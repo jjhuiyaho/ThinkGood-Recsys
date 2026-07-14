ec3bdbada4757ec68ce39ae49f9ac63d5b5bda0a
HanJaehui <han.zinna@gmail.com>
2026-07-14 10:40:15 +0900

    Update recommend.py

--- thinkgood-web/api/recommend.py
+++ thinkgood-web/api/recommend.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import os
-from recommender import Recommender
+from api.recommender import Recommender
 
 app = FastAPI()
 
